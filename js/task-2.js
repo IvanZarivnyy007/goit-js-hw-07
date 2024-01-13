138e0f3fb06aa7b9eb04713dbd9705420e61ae3d
IvanZarivnyy007 <144140704+IvanZarivnyy007@users.noreply.github.com>
2024-01-14 00:34:12 +0100

    3
    3

--- js/task-2.js
+++ js/task-2.js
@@ -1,29 +1,29 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://cdn.27.ua/sc--media--prod/default/16/e9/82/16e98218-1842-4677-a580-138bbb614621.jpg',
     alt: 'White and Black Long Fur Cat',
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://armyinform.com.ua/wp-content/uploads/2022/09/f_19230554251646029050.jpg',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://karpaty-ua.org.ua/images/obychai_tradicii/slava-ukraini.jpg',
     alt: 'Group of Horses Running',
   },
   {
     url:
-      'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+      'https://www.rbc.ua/static/ckef2/img/Screenshot_34_14.png',
     alt: 'Elephant Beside on Baby Elephant',
   },
   {
     url:
-      'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+      'https://fckryvbas.com/storage/2023-12/den-zsu-na-sait-1701849164iwfQU.jpg',
     alt: 'Blue Geeen and Orange Parrot',
   },
   {
     url:
-      'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Stamp_of_Ukraine_s1985.jpg/1200px-Stamp_of_Ukraine_s1985.jpg',
     alt: 'Zebras on Zebra',
   },
 ];
@@ -32,12 +32,18 @@ const images = [
 const galeryImages = document.querySelector('ul');
 
 
+
 images.forEach(image => {
   const newImg = document.createElement('img')
   newImg.setAttribute('src', image.url)
   newImg.setAttribute('alt', image.alt)
+  newImg.setAttribute('width', '500');
+  newImg.setAttribute('height', '350');
+  newImg.style.borderRadius = '10px';
+  newImg.style.boxShadow = '9px 9px yellow, -1em 0 .4em blue';
   const newLi = document.createElement('li');
   newLi.appendChild(newImg)
   galeryImages.appendChild(newLi);
 
 })
+
